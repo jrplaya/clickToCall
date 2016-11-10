800ab2dc504d59951409133069e084d7482ebdf1
jrplaya <jeff@jeffrojas.com>
2016-11-10 13:19:34 -0800

    file for click to talk

---
+++ click-to-talk.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Click to Call
+ * Plugin URI: https://www.jeffrojas.com/
+ * Description: Let your customers call you from your website using the click to call button.
+ * Version: 1.0
+ * Author: Jeff Rojas
+ * Author URI: http://jeffrojas.com/
+ **/
+?>
